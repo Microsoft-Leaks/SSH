eb93ab4c781d31d9672c78fb9850e3254c66bbc7
Nippon <37734266+nnippon@users.noreply.github.com>
2021-01-08 16:50:00 +0200

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,1 +1,6 @@
-
+wget https://raw.githubusercontent.com/rokibhasansagar/slimhub_actions/main/cleanup.sh; bash cleanup.sh -y
+sudo -E apt-get -qq update
+sudo -E apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev rsync flex bison ccache openjdk-8-jdk expect aria2
+git config --global user.name "nnippon"
+git config --global user.email "adameyyad999@gmail.com"
+git clone --recurse-submodules https://github.com/nnippon/ErfanGSIs
