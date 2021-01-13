8c4781bdba6680704af496dfa2d6ad8df2fe6bec
Wino.Ltd <72581300+winoproject@users.noreply.github.com>
2021-01-13 16:19:04 +0530

    Update ubuntu-run.sh

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -3,4 +3,4 @@ sudo -E apt-get -qq update
 sudo -E apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev rsync flex bison ccache openjdk-8-jdk expect aria2
 git config --global user.name "Wino"
 git config --global user.email "secrets.EMAILGIT"
-git clone --recurse-submodules https://github.com/nnippon/ErfanGSIs
+git clone --recurse-submodules https://github.com/winoproject/ErfanGSIs
